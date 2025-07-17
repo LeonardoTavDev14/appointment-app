e3ef90cb6fb11bac8c950de3fff52c6d0ef5094a
LeonardoTavDev14 <contatoleonardotavdev@gmail.com>
2025-07-17 20:28:21 -0300

    feat: fixing-routes-code

--- src/http/routes/user.routes.ts
+++ src/http/routes/user.routes.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 
-// MIDDLEWARES
 import { ensureRole } from "../middlewares/ensureRole";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
-// IMPORT DAS CONTROLLERS
 import { CreateUserController } from "../controllers/user/CreateUserController";
 import { AuthUserController } from "../controllers/user/AuthUserController";
 import { FindManyUsersController } from "../controllers/user/FindManyUsersController";
@@ -13,18 +11,15 @@ import { DeleteUserController } from "../controllers/user/DeleteUserController";
 
 const routes = Router();
 
-// INSTÂNCIAS CONTROLLERS
 const createUserController = new CreateUserController();
 const authUserController = new AuthUserController();
 const findManyUsersController = new FindManyUsersController();
 const updateUserController = new UpdateUserController();
 const deleteUserController = new DeleteUserController();
 
-// POST
 routes.post("/created", createUserController.handle);
 routes.post("/login", authUserController.handle);
 
-// GET
 routes.get(
   "/findmany",
   ensureAuthenticated,
@@ -32,10 +27,8 @@ routes.get(
   findManyUsersController.handle
 );
 
-// PUT
 routes.put("/updated", ensureAuthenticated, updateUserController.handle);
 
-// DELETE
 routes.delete("/deleted", ensureAuthenticated, deleteUserController.handle);
 
 export { routes as userRoutes };
